20cabd65cd0a942c3268449f8067152471331531
HarryCole0 <95872572+HarryCole0@users.noreply.github.com>
2021-12-10 15:53:38 +0000

    Add files via upload
    Thsi should be the code

---
+++ main.py
@@ -0,0 +1,75 @@
+from tkinter import *
+
+root = Tk()
+root.title("Sudoku Solver")
+root.geometry("800x800")
+
+label = Label(root, text="Fill the numbers in and to solve press SOLVE").grid(row=0, column=1, columnspan=10)
+
+
+errLabel = Label(root, text="", fg="red")
+errLabel.grid(row=15, column=1, columnspan=10, pady=5)
+
+solvedLabel= Label(root, text="", fg="green")
+solvedLabel.grid(row=15, column=1, columnspan=10, pady=5)
+
+cells = {}
+
+def ValidateNumber(P):
+    out=(P.isdigit() or P=="") and len(P)<2
+    return out
+
+reg = root.register(ValidateNumber)
+
+
+def draw3x3Grid(row, column, bgcolor):
+    for i in range(3):
+        for j in range(3):
+            e = Entry(root, width=5, bg= bgcolor, justify="center", validate="key", validatecommand=(reg, "%P"))
+            e.grid(row=row+i+1, column=column+j+1, sticky="nsew", padx=1, ipady=5)
+            cells[(row+i+1, column+j+1)] = e
+
+def draw9x9Grd():
+    color="#D0ffff"
+    for rowNo in range(1, 10, 3):
+        for colNo in range(0, 9, 3):
+            draw3x3Grid(rowNo, colNo, color)
+            if color== "#D0ffff":
+                color="#ffffd0"
+            else:
+                color="#D0ffff"
+
+
+def clearValues():
+    errLabel.configure(text="")
+    solvedLabel.configure(text="")
+    for row in range(2, 11):
+        for col in range(1, 10):
+            cell = cells[(row, col)]
+            cell.delete(0, "end")
+
+
+def getValues():
+    board=[]
+    errLabel.configure(text="")
+    solvedLabel.configure(text="")
+    for row in range(2,11):
+        rows = []
+        for col in range(1,10):
+            val = ""
+            if val =="":
+                rows.append(0)
+            else:
+                rows.append(int(val))
+
+        board.append(rows)
+
+
+btn = Button(root, command=getValues, text="Solve", width=10)
+btn.grid(row=20, column=1, columnspan=5, pady=20)
+
+btn = Button(root, command=getValues, text="Solve", width=10)
+btn.grid(row=20, column=1, columnspan=5, pady=20)
+
+draw9x9Grd()
+root.mainloop()
